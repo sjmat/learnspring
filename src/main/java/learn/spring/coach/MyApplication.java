9febc4e2513544b073fe671ed8ef20075bbd5e60
sjm <sony.j.m.17@gmail.com>
2019-02-22 00:29:53 +0530

    Added applicationOCntext.xml Added Logger

--- src/main/java/learn/spring/coach/MyApplication.java
+++ src/main/java/learn/spring/coach/MyApplication.java
@@ -1,5 +1,7 @@
 package learn.spring.coach;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApplication {
 
 	public static void main(String[] args) {
@@ -8,6 +10,11 @@ public class MyApplication {
 		
 		System.out.println(coach1.getMyAdvice());
 		System.out.println(coach2.getMyAdvice());
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ICoach coach3 = context.getBean("coach", ICoach.class);
+		System.out.println(coach3.getMyAdvice());
+		context.close();
 	}
 
 }
